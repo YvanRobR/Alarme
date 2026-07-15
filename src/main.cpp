1a3f654ae5529765846f4ea04092744d10e6089d
YvanRobR <yvan.rob.r@gmail.com>
2026-07-15 18:30:26 +0200

    Modif CMakeLists.txt et main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 
-int main(int argc,char *argv[])
+#include "AlarmManager.h"
+
+int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc,argv);
+    QGuiApplication app(argc, argv);
 
     app.setApplicationName("Alarme");
     app.setOrganizationName("YvanRobR");
 
+    AlarmManager alarmManager;
+
     QQmlApplicationEngine engine;
 
-    engine.load(QUrl("qrc:/qml/Main.qml"));
+    engine.rootContext()->setContextProperty("alarm", &alarmManager);
 
-    if(engine.rootObjects().isEmpty())
+    engine.load(QUrl("qrc:/Main.qml"));
+
+    if (engine.rootObjects().isEmpty())
         return -1;
 
     return app.exec();
